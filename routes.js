f12c1de23a2c7468eba1fdc35ffb9b373bbabc17
Andy Pellett <anrope@gmail.com>
2012-11-10 21:11:23 -0500

    unbroke stuff

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 var index = require('./index');
 var dropgif = require('./dropgif');
 var song = require('./song');
-var fetchgif = require('./fetchgif');
+//var fetchgif = require('./fetchgif');
 
 module.exports = function(app) {
   app.all('/', index);
   app.post('/1/dropgif/:song_name/', dropgif);
   app.get('/1/song/:song_name/', song);
-  app.get('/1/fetchgif', fetchgif);
+  //app.get('/1/fetchgif', fetchgif);
 }
