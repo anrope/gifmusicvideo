5d3f7876fdddf415ef02df1ada8cfafce3fee636
Andy Pellett <anrope@gmail.com>
2012-11-11 00:07:16 -0500

    Working on the api calls for the song view

--- play.js
+++ play.js
@@ -1,3 +1,4 @@
 module.exports = function(req, res) {
-    res.sendfile(__dirname + '/public/play.html');
+    //res.sendfile(__dirname + '/public/play.html');
+    res.render('play');
 };
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,12 +1,14 @@
 function PlayerCtrl($scope, $http) {
     // Get GIF data for the song
-    $http.get(appConfig.context + '/1/song/').success(function(data) {
-        $scope.phones = data;
-    });
+    //$http.get(appConfig.context + '/1/song/').success(function(data) {
+    //    $scope.phones = data;
+    //});
 
 
     // Get GIF data for the song
-    $http.get(appConfig.context + '/1/fetchgifs/').success(function(data) {
+    $http.get(appConfig.context + '/1/suggest/anrope/gangnam-style').success(function(data) {
+        console.log(data);
+        console.log('lol');
         $scope.suggest = data;
     });
 
