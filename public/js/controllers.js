2c24c2afe3dc3fecbf061a9708c55bc1479b153d
Albert Nazander <albert@thinglink.com>
2012-11-11 01:43:39 -0500

    player working

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,5 +1,12 @@
 function PlayerCtrl($scope, $http) {
-    
+    var m = window.location.pathname.match(/\/song\/([^\/]+)\/([^\/]+)/);
+    if (m.length < 2) {
+        alert('Invalid song path')
+        return;
+    }
+    var user = m[1];
+    var songname = m[2];
+    console.log(user, songname);
     // Get GIF data for the song
     //$http.get(appConfig.context + '/1/song/').success(function(data) {
     //    $scope.phones = data;
@@ -7,7 +14,10 @@ function PlayerCtrl($scope, $http) {
 
 
     // Get GIF data for the song
-    $http.get(appConfig.context + '/1/fetchgifs/').success(function(data) {
+    song_api = appConfig.context + '/1/suggest/' + user + "/" + songname + "/";
+    $http.get(song_api).success(function(data) {
+        console.log(data);
+        console.log('lol');
         $scope.suggest = data;
     });
 
