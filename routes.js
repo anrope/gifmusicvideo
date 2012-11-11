f0fc40f27979194ba07ecb9535930d8d9163cf51
Andy Pellett <anrope@gmail.com>
2012-11-10 23:23:37 -0500

    Renamed fetchgif endpoint to suggest

--- routes.js
+++ routes.js
@@ -2,12 +2,12 @@ var index = require('./index');
 var dropgif = require('./dropgif');
 var song = require('./song');
 var play = require('./play')
-var fetchgif = require('./fetchgif');
+var suggest = require('./suggest');
 
 module.exports = function(app) {
   app.all('/', index);
   app.post('/1/dropgif/:song_name', dropgif);
   app.get('/1/song/:song_name', song);
   app.get('/song/:soundcloud_user/:song_name', play);
-  app.get('/1/fetchgif', fetchgif);
+  app.get('/1/suggest', suggest);
 }
