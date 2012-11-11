81c308b2954aac035bcbd71e913ff7e316988e67
Albert Nazander <albert@thinglink.com>
2012-11-11 01:48:03 -0500

    suggestion strip working

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,12 +1,12 @@
 window.appConfig = {};
-if (window.location.host.indexOf('localhost') != -1) {
-    appConfig.env = 'local';
-    appConfig.context = 'djg.jit.su';
-    appConfig.scClientId = 'b60ac37afa8549e97f3d5e7d0fce15f5';
-    appConfig.scRedirectURI = 'http://localhost:8087/callback.html';
-} else {
+if (window.location.host.indexOf('localhost') == -1) {
     appConfig.env = 'production';
-    appConfig.context = 'localhost:8087';
+    appConfig.context = 'djg.jit.su';
     appConfig.scClientId = '';
     appConfig.scRedirectURI = '';
+} else {
+    appConfig.env = 'local';
+    appConfig.context = 'localhost:8087';
+    appConfig.scClientId = 'b60ac37afa8549e97f3d5e7d0fce15f5';
+    appConfig.scRedirectURI = 'http://localhost:8087/callback.html';
 }
