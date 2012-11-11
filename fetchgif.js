88dc7e69f7f4e54e8e7bd4dfc0dcc288e1058d08
Andy Pellett <anrope@gmail.com>
2012-11-10 21:53:49 -0500

    Added stupid simple fetchgif endpoint

--- fetchgif.js
+++ fetchgif.js
@@ -1,31 +1,5 @@
-var storage = require('./storage')
+var s3_gifs = require('./s3_gifs.json')
 
 module.exports = function (req, res) {
-    rds = GLOBAL.rds;
-
-    user = req.body.user;
-    gif_file = req.body.gif;
-    timestamp = req.body.timestamp;
-    row = req.body.row;
-
-    gif = {
-        'user': user,
-        'gif': gif_file,
-        'timestamp': timestamp,
-        'row': row
-    };
-
-    song_name = req.params.song_name;
-    song_key = storage.songMakeKey(song_name);
-    rds.get(song_key, function (err, song) {
-        song = JSON.parse(song)
-        conflict = logic.checkOverlap(gif, song)
-        if (!conflict) {
-            logic.integrateGIF(gif, song);
-            song = JSON.stringify(song);
-            rds.set(song_key, song);
-        }
-
-        res.send('true\n');
-    });
+    res.send(s3_gifs);
 };
