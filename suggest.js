161f87f5bbd9304e39ca60f72ac4105537780730
Andy Pellett <anrope@gmail.com>
2012-11-11 14:19:54 -0500

    Special suggestions working

--- suggest.js
+++ suggest.js
@@ -1,15 +1,38 @@
 var JSON = require('JSON')
 
 var images = require('./images.json')
+var specials = {
+    '50-cent': require('./50-cent.json'),
+    'bad-girls': require('./bad-girls.json'),
+    'paper-planes': require('./paper-planes.json'),
+    'zedd': require('./zedd.json')
+};
 
 var base_url = "http://alizweb.s3.amazonaws.com/gifmusicvideo/"
 
 module.exports = function (req, res) {
+    user = req.params.user;
+    song_name = req.params.song_name;
+
+    suggest_images = images
+
+    console.log(Object.keys(specials))
+
+    for (sp in specials) {
+        console.log(sp)
+        if (song_name.indexOf(sp) != -1) {
+            suggest_images = specials[sp]
+            break;
+        }
+    }
+
+    console.log(suggest_images)
+
     suggestions = []
 
     for (var i = 0; i < 10; i++) {
-        index = Math.floor(Math.random() * images.length);
-        suggestions.push(images[index]);
+        index = Math.floor(Math.random() * suggest_images.length);
+        suggestions.push(suggest_images[index]);
     }
 
     gifs = {
