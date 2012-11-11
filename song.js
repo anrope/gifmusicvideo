f5967c2007e2e13c0072cd85ec779726e268c373
Andy Pellett <anrope@gmail.com>
2012-11-10 20:36:30 -0500

    Set up dev/production environments

--- song.js
+++ song.js
@@ -1,10 +1,24 @@
+var JSON = require('JSON')
+
 var storage = require('./storage')
 
 module.exports = function(req, res) {
     // Get gif placement info from the backend for a song
     rds = GLOBAL.rds;
-    song = req.params.song_id;
-    rds.get(storage.songMakeKey(song), function (err, reply) {
-        res.send(reply)
+    song_id = req.params.song_id;
+    song_key = storage.songMakeKey(song_id);
+    rds.get(song_key, function (err, song) {
+        if (!song) {
+            song = JSON.stringify({
+                'project_id': 'whatever',
+                'soundcloud_id': 2341345,
+                'gifs': []
+            });
+
+            //rds.set(song_key, song, function (err) {
+            //}
+        } else {
+            res.send(song)
+        }
     });
 };
